509a29a88d1a59a0cfc3d4c48da143fc932d58f9
Andreas Barthel <uprightcitizen@gmx.de>
2020-12-08 14:47:26 +0100

    .gitignore, gitlab-provision, how to angepasst

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 # hier kommt später noch was rein.
+apt-get update
+apt-get upgrade # kein full-upgrade, da wenn jetzt kernel-Änderungen wären, müssten wir neu starten. also upgrade ohne mögliche Kernel-Updates.
