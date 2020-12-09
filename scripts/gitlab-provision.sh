0d416fd4175a67c4635a751c0a644bc89916c970
inconsistencies78 <uprightcitizen@gmx.de>
2020-12-09 12:43:44 +0100

    provision - Docker installiert

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -2,3 +2,10 @@
 # hier kommt später noch was rein.
 apt-get update
 apt-get -y upgrade # kein full-upgrade, da wenn jetzt kernel-Änderungen wären, müssten wir neu starten. also upgrade ohne mögliche Kernel-Updates.
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common # vorbereiten, um aktuelle Docker Variante u.a. zu installieren
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - # docker gpg Schlüssel hinzufügen
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # das Docker repository hinzufügen. $(lsb_release -cs) gibt den Kurznamen der aktuelen Ubuntu-Variante aus. damit sind wir für ubuntu-updates gerüstet
+apt-get update 
+apt-get -y install docker-ce docker-ce-cli containerd.io 
+usermod -aG docker vagrant 
+systemctl enable docker #docker wird bei jedem boot automatisch gestartet
